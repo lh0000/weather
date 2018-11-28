009a546c2abf9697e717957461987556785766c3
lihan <921193967@qq.com>
2018-11-28 11:07:45 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/lihan/coolweather/db/County.java
@@ -0,0 +1,45 @@
+package com.example.lihan.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+
+}
